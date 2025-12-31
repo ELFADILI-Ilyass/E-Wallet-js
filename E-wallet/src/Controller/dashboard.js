04fc164e95f6d772941e149e405785d8560d1b89
Ilyass EL FADILI <elfadili04ilyass@gmail.com>
2025-12-31 03:21:12 +0100

    adding a payement method
    Added payment functionality and transaction updates.

--- E-wallet/src/Controller/dashboard.js
+++ E-wallet/src/Controller/dashboard.js
@@ -1,24 +1,18 @@
-import { FindTransaction } from "../Model/data.js";
+import { FindTransaction, FindUser } from "../Model/data.js";
+
 const welcome_message=document.getElementById("welcome_message");
 const montant=document.getElementById("balance");
 const date=document.getElementById("date");
 const transac=document.getElementById("transactions");
 const filterS=document.getElementById("filterX");
+const transfer=document.getElementById("transferer");
+const payerbtn=document.getElementById("payer");
+
 
 const user = JSON.parse(sessionStorage.getItem("user"));
 welcome_message.textContent ="Bonjour " + user.nom;
 montant.textContent=user.balance+"  MAD";
-// user.transaction.forEach(t => {
-//     const tr = document.createElement("tr");
-//     tr.innerHTML = `
-//         <td>${t.date}</td>
-//         <td>${t.description}</td>
-//         <td>${t.type}</td>
-//         <td>${t.montant}</td>
-//     `;
 
-//     transactions.appendChild(tr);
-// });
 const allTransactions=user.transaction;
 
 function returnF(tab) {
@@ -57,6 +51,103 @@ filterS.addEventListener("change",()=>{
         
       }
     });
+ 
+    
+    
+    //payer
+
+
+
+payerbtn.addEventListener("click",()=>{
+    const isHidden = paymentContainer.style.display === "none";
+    paymentContainer.style.display = isHidden ? "block" : "none";
+
+})
+
+const confirmBtn =document.getElementById("Confirm");
+const status = document.getElementById("paymentStatus");
+
+const checkSolde=(prix)=>{
+  return new Promise((resolve,reject)=>{
+    if(user.balance>=prix && prix>0){
+      resolve('Success');
+    }
+    else if (prix <= 0) {
+            reject("Montant invalide");
+        } else {
+            reject("Solde insuffisant");
+        }
+  })
+}
+
+
+const checkUser=(inputPassword)=>{
+   return new Promise ((resolve,reject)=>{
+    setTimeout(()=>{
+
+      if(!user){
+          reject(" Utulisateur introuvable");
+      }
+      else if(inputPassword!==user.password)
+      {
+        reject("Mot de pass incorrect");
+          
+      }
+      else{
+        resolve("Bonjour "+user.nom);
+      }
+   },1000)
+    })  
+  
+  }
+
+  const updateData = (amount) => {
+    user.balance -= amount;
+
+    const newT = {
+        date:"2025-12-31" ,
+        description: "Paiement Service",
+        type: "-",
+        montant: amount
+    };
+    user.transaction.push(newT);
+    sessionStorage.setItem("user", JSON.stringify(user));
+
+    montant.textContent = user.balance + " MAD";
+    
+    returnF(user.transaction); 
+};
+
+confirmBtn.addEventListener("click",()=>{
+  const montant= parseFloat(document.getElementById("prixInput").value);
+  const pass= document.getElementById("passInput").value;
+
+  status.textContent="Verification...."
+  status.style.color="orange";
+
+  checkUser(pass)
+    .then((message)=>{
+      status.textContent=message+"  Verification du solde...";
+      return checkSolde(montant);
+    })
+    .then(() => {
+
+            updateData(montant);
+
+            status.textContent = "Paiement réussi !";
+            status.style.color = "green";
+        })
+        .catch((error) => {
+            status.textContent = error;
+            status.style.color = "red";
+        });
+
+});
+
+
+
+
+
 
 
 //     function updateClock() {
@@ -68,7 +159,17 @@ filterS.addEventListener("change",()=>{
 // // Start the timer
 // setInterval(updateClock, 1000);
 // updateClock();
+// user.transaction.forEach(t => {
+//     const tr = document.createElement("tr");
+//     tr.innerHTML = `
+//         <td>${t.date}</td>
+//         <td>${t.description}</td>
+//         <td>${t.type}</td>
+//         <td>${t.montant}</td>
+//     `;
 
+//     transactions.appendChild(tr);
+// });
 function updateDate() {
     const now = new Date();
     
